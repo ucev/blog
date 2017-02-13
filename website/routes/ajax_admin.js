aacfed69e11f3b180f641e27ef2a00b253f60a83
ucev <zhangshuaiyf@icloud.com>
2017-02-13 10:06:02 +0800

    progressing, article filter

--- website/routes/ajax_admin.js
+++ website/routes/ajax_admin.js
@@ -7,6 +7,14 @@ function strToNum(str) {
   return isNaN(a) ? 0 : a;
 }
 
+function emptyString(str) {
+  console.log('-----------str start');
+  //str = str.trim();
+  console.log('str: ' + str);
+  var str =  (str === null || str === undefined) ? '' : str.trim();
+  return (str === '' || str === '-1') ? '' : str;
+}
+
 router.get('/articles/modify', (req, res, next) => {
   const id = strToNum(req.query.id);
   const state = req.query.state;
@@ -29,15 +37,38 @@ router.get('/articles/modify', (req, res, next) => {
 });
 
 router.get('/articles/get', (req, res, next) => {
+  console.log('------------------');
+  console.log(req.query);
+  console.log('------------------');
   const current = strToNum(req.query.start);
   const start = current * configs.query_config.step;
+  const state = emptyString(req.query.state);
+  const category = strToNum(emptyString(req.query.category));
+  const label = emptyString(req.query.label);
   const mysql = require('mysql');
   const conn = mysql.createConnection(configs.database_config);
-  conn.query('select count(*) as total from articles', (err, results, fields) => {
+  var wheres = ' where 1';
+  var whereArgs = [];
+  if ( state || category || label) {
+    if (state) {
+      wheres += ' AND state = ?';
+      whereArgs.push(state);
+     }
+    if (category) {
+      wheres += ' AND category = ?';
+      whereArgs.push(category);
+    }
+    if (label) {
+      wheres += ' AND label = ?';
+      whereArgs.push(label);
+    }
+  }
+  conn.query('select count(*) as total from articles' + wheres, whereArgs, (err, results, fields) => {
     const total = Math.ceil((results[0]['total'] + 1)/ configs.query_config.step);
-    conn.query('select id, title, category, label, state, top, pageview from articles limit ?, ?',
-      [start, configs.query_config.step],  
-      (err, results, fields) => {
+    var sql = 'select id, title, category, label, state, top, pageview from articles ';
+    sql += wheres;
+    sql += ' limit ?, ?';
+    conn.query(sql, [...whereArgs, start, configs.query_config.step], (err, results, fields) => {
         res.json({code: 0, msg: '获取成功', data: {
           total: total,
           current: current,
