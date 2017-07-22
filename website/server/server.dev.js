7fea31fab1d2f7abc6d8d12679ba2f70c6ca5724
ucev <zhangshuaiyf@icloud.com>
2017-07-22 16:06:46 +0800

    add webpack.config file

--- website/server/server.dev.js
+++ website/server/server.dev.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const webpackDevServer = require('webpack-dev-server');
-const webpackConfig = require(path.join(__dirname, '../../webpack.config.dev'));
+const webpackConfig = require(path.join(__dirname, '../../webpack.dev.config'));
 
 const compiler = webpack(webpackConfig);
 const server = new webpackDevServer(compiler, {
